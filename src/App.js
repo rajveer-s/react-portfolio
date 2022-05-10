a947026f6ba375523bdc6766798d9eb1cffa6ba8
Rajveer Singh <rajsidhu2924@gmail.com>
2022-05-10 13:11:47 -0700

    router-dom/routes for about page added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import About from './pages/about/About';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <About />
-      <Footer />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/about'
+            element={<About />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
