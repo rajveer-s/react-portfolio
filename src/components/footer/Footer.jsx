b1510277e14cc265b4b03bf96158cf0c267302b2
Rajveer Singh <rajsidhu2924@gmail.com>
2022-05-09 23:34:59 -0700

    footer addded

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <div id="footer">
+            Thank you !
+        </div>
+    </div>
+  )
+}
+
+export default Footer
