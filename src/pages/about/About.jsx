70efeef9d3117c7e31691364f6fcb8f2c0da3f5f
Rajveer Singh <rajsidhu2924@gmail.com>
2022-05-11 00:49:29 -0700

    deploy

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -7,7 +7,8 @@ const About = () => {
         <div id="aboutPic">
           <img src='https://bootcampspot.com/broker/studentAvatar?accountId=102745' alt='Profile pic' />
         </div>
-        <p> Hello there, My name is Rajveer Singh. I am a Full stack developer certified from UCLA extension. I love working with MERN stack. I am dedicated to the quest of developing solutions that leverage on best practice technologies to develiver over the top user experience.   </p>
+        <p> Hello there, My name is Rajveer Singh. I am a Full stack developer certified from UCLA extension. I love working with MERN stack. I am dedicated to the quest of developing solutions that leverage on best practice technologies to develiver over the top user experience.</p>
+        <div id='resumeDiv'><a href="#" download>RESUME</a></div>
     </div>
   )
 }
