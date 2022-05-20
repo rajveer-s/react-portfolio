f1d456050014b18d8d435f468adc33d18d3d62d3
Rajveer Singh <rajsidhu2924@gmail.com>
2022-05-20 14:39:57 -0700

    skills added

--- src/pages/skills/Skills.jsx
+++ src/pages/skills/Skills.jsx
@@ -4,24 +4,59 @@ const Skills = () => {
   return (
     <div>
       <section id="skills">
-    <h2>Skills</h2>
-    <div id="icons">
-        <div>
+        <h2>Skills</h2>
+        <div id="icons">
+          <div>
             <i class="devicon-html5-plain-wordmark colored"></i>
+          </div>
+          <div><i class="devicon-css3-plain-wordmark colored"></i>
+          </div>
+          <div><i class="devicon-javascript-plain colored"></i>
+          </div>
+          <div><i class="devicon-react-original-wordmark colored"></i>
+          </div>
+          <div><i class="devicon-bootstrap-plain-wordmark colored"></i>
+          </div>
+          <div><i class="devicon-git-plain-wordmark colored"></i>
+          </div>
+          <div><i class="devicon-nodejs-plain-wordmark colored"></i>
+          </div>
+          <div>
+            <i class="devicon-figma-plain colored"></i>
+          </div>
+          <div>
+            <i class="devicon-github-original-wordmark colored"></i>
+          </div>
+          <div><i class="devicon-graphql-plain-wordmark colored"></i>
+          </div>
+          <div>
+          <i class="devicon-handlebars-plain-wordmark"></i>
+          </div>
+          <div>
+          <i class="devicon-heroku-plain-wordmark colored"></i>
+          </div>
+          <div>
+          <i class="devicon-jquery-plain-wordmark colored"></i>
+          </div>
+          <div>
+          <i class="devicon-mongodb-plain-wordmark colored"></i>
+          </div>
+          <div>
+          <i class="devicon-mysql-plain-wordmark colored"></i>
+          </div>
+          <div>
+          <i class="devicon-sequelize-plain-wordmark colored"></i>
+          </div>
+          <div>
+          <i class="devicon-slack-plain-wordmark colored"></i>
+          </div>
+          <div><i class="devicon-vscode-plain-wordmark colored"></i>
+          </div>
+          
+
         </div>
-        <div><i class="devicon-css3-plain-wordmark colored"></i>
-        </div>
-        <div><i class="devicon-javascript-plain colored"></i>
-        </div>
-        <div><i class="devicon-bootstrap-plain-wordmark colored"></i>
-        </div>
-        <div><i class="devicon-git-plain-wordmark colored"></i>
-        </div>
-        <div><i class="devicon-nodejs-plain-wordmark colored"></i>
-        </div>
+      </section>
     </div>
-</section>
-</div>
   )
 }
 
