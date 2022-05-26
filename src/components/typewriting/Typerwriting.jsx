504aa9d9c4f220e956ecbac9a526aa5f089c8bf8
Rajveer Singh <rajsidhu2924@gmail.com>
2022-05-26 15:06:43 -0700

    usetypewriter object added

--- src/components/typewriting/Typerwriting.jsx
+++ src/components/typewriting/Typerwriting.jsx
@@ -3,6 +3,14 @@ import { useTypewriter, Cursor} from 'react-simple-typewriter'
 
 const HeaderEffect = () => {
 
+  const {text} = useTypewriter({
+    words: ['Web Developer', 'Critical Thinker', 'Creative' ],
+    loop: true,
+    delaySpeed: 2000,
+    deleteSpeed: 90,
+    typeSpeed: 100
+  })
+
   return (
     <div className='loopdiv'>
       <h5>{text} <Cursor /></h5>
