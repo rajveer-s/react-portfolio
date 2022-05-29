b0a1c11d6539bd0f1a02b74435f2f7d48401df89
Rajveer Singh <rajsidhu2924@gmail.com>
2022-05-28 20:31:54 -0700

    resume added for download

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
           <img src='https://bootcampspot.com/broker/studentAvatar?accountId=102745' alt='Profile pic' />
         </div>
         <p> Hello there, My name is Rajveer Singh. I am a Full stack developer certified from UCLA extension. I specialize in JavaScript. I have worked with technologies such as Handlebars, Express JS, MonogoDB, Git, and Herkou. I also have experience working with React. I am dedicated to the quest of developing solutions that leverage on best practice technologies to develiver over the top user experience.</p>
-        <div id='resumeDiv'><a href="/" download>RESUME</a></div>
+        <div id='resumeDiv'><a href="../public/resume.dpf" download>RESUME</a></div>
     </div>
   )
 }
