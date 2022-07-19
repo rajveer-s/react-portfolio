213e46fee9326d8d675556e5bbf75beb6420abdf
Rajveer Singh <rajsidhu2924@gmail.com>
2022-07-18 18:50:31 -0700

    img update

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
       <div id="aboutPic"
         data-aos="fade-left"
       >
-        {/* upload an acuatal pic of myself  */}
+        {/* selfie update  */}
         <img src='https://bootcampspot.com/broker/studentAvatar?accountId=102745' alt='Profile pic' />
       </div>
       <p data-aos="fade-right"> Hello there, My name is Rajveer Singh. I am a Full stack developer certified from UCLA extension. I specialize in JavaScript. I have worked with technologies such as Handlebars, Express JS, MonogoDB, Git, and Herkou. I also have experience working with React. I am dedicated to the quest of developing solutions that leverage on best practice technologies to develiver over the top user experience.</p>
