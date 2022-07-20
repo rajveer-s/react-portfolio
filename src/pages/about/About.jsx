0c344dbdc45d7cd90d8bede80e7d1377b19be531
Rajveer Singh <rajsidhu2924@gmail.com>
2022-07-20 00:23:40 -0700

    actual pic udpated

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import aboutImg from '../../assets/images/IMG.png'
 
 
 AOS.init({
@@ -16,7 +17,7 @@ const About = () => {
         data-aos="fade-left"
       >
         {/* selfie update  */}
-        <img src='https://bootcampspot.com/broker/studentAvatar?accountId=102745' alt='Profile pic' />
+        <img src={aboutImg} alt='Profile pic' />
       </div>
       <p data-aos="fade-right"> Hello there, My name is Rajveer Singh. I am a Full stack developer certified from UCLA extension. I specialize in JavaScript. I have worked with technologies such as Handlebars, Express JS, MonogoDB, Git, and Herkou. I also have experience working with React. I am dedicated to the quest of developing solutions that leverage on best practice technologies to develiver over the top user experience.</p>
       <div id='resumeDiv'><a href="../public/resume.pdf" download>RESUME</a></div>
