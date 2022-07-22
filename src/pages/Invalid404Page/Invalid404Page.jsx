a279b31992709b03e7566108ebea906f96f30aaa
Rajveer Singh <rajsidhu2924@gmail.com>
2022-07-21 17:50:51 -0700

    404 page edited

--- src/pages/Invalid404Page/Invalid404Page.jsx
+++ src/pages/Invalid404Page/Invalid404Page.jsx
@@ -4,6 +4,7 @@ import { AiOutlineArrowLeft } from 'react-icons/ai'
 const Invalid404Page = () => {
   return (
     <div>
+      {/* re-create the 404 page  */}
       <h3 style={{paddingTop: "20px"}}>This page doesn't exist</h3>
       <a style={{fontSize: "2rem", textDecoration:"none"}} href="/"><AiOutlineArrowLeft />About Me</a>
     </div>
